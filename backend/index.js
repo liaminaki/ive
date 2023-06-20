9c7c032297c1af449211e65a2feb5643679542bf
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2023-06-20 17:03:25 +0800

    Connect ive database to backend

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,13 @@ import express from "express"   // Build APIs
 import mysql from "mysql"       // Database
 import cors from "cors"         // Allow requests from other origins
 import multer from "multer"     // Allow uploading files
+
+// Connect to Database
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"pass123",
+    database:"ive"
+})
+
+
