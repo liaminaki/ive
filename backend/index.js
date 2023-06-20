5d1745129f1bffc134a7dd7139ea9abe4dcf1d3e
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2023-06-20 17:00:38 +0800

    Install mysql, cors, multer

---
+++ backend/index.js
@@ -0,0 +1,4 @@
+import express from "express"   // Build APIs
+import mysql from "mysql"       // Database
+import cors from "cors"         // Allow requests from other origins
+import multer from "multer"     // Allow uploading files
