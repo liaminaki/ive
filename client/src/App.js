599a9d7f9020081724f3bef23a2ece9d3b01e11e
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2023-06-20 17:37:26 +0800

    Set up pages for adding album

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route,} from "react-router-dom";
+import Album from './pages/Album';
+import AddAlbum from './pages/AddAlbum';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/discography" element={<Album/>}/>
+          <Route path="/add" element={<AddAlbum/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
